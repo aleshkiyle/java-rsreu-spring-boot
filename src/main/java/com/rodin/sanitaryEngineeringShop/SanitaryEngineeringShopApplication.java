503c7d920d2de1de2cb1af09ecd77cbdf6fdd2ae
aleshkiyle <aleksey.rodin62@gmail.com>
2022-09-27 20:40:38 +0300

    remove unnecessary files

--- src/main/java/com/rodin/sanitaryEngineeringShop/SanitaryEngineeringShopApplication.java
+++ src/main/java/com/rodin/sanitaryEngineeringShop/SanitaryEngineeringShopApplication.java
@@ -1,25 +1,12 @@
 package com.rodin.sanitaryEngineeringShop;
 
-import com.rodin.sanitaryEngineeringShop.configuration.JDBCPostgreConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SanitaryEngineeringShopApplication implements CommandLineRunner {
-
-    private final JDBCPostgreConfig jdbcPostgreConfig;
-
-    public SanitaryEngineeringShopApplication(JDBCPostgreConfig jdbcPostgreConfig) {
-        this.jdbcPostgreConfig = jdbcPostgreConfig;
-    }
+public class SanitaryEngineeringShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SanitaryEngineeringShopApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        jdbcPostgreConfig.addStudent();
     }
 }
