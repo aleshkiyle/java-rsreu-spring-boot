92fcecd47e5d12ce117e60c90073c218de61c74f
aleshkiyle <aleksey.rodin62@gmail.com>
2022-10-21 19:50:21 +0300

    fix classes

--- src/main/java/com/rodin/sanitaryEngineeringShop/configuration/SecurityConfig.java
+++ src/main/java/com/rodin/sanitaryEngineeringShop/configuration/SecurityConfig.java
@@ -1,20 +1,9 @@
 package com.rodin.sanitaryEngineeringShop.configuration;
 
-import com.rodin.sanitaryEngineeringShop.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 
 @Configuration
 public class SecurityConfig {
@@ -24,29 +13,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // применяет надёжное шифрования bcrypt;
-    }
-
-    @Bean
-    public UserDetailService userDetailService(PasswordEncoder passwordEncoder) {
-        List<UserDetails> usersList = new ArrayList<>();
-        usersList.add(
-                new User("aleshkiyle", passwordEncoder.encode("password"),
-                        List.of(new SimpleGrantedAuthority("ROLE_USER"))));
-        usersList.add(
-                new User("frs717", passwordEncoder.encode("password"),
-                        List.of(new SimpleGrantedAuthority("ROLE_USER"))));
-        return (UserDetailService) new InMemoryUserDetailsManager(usersList);
-    }
-
-    @Bean
-    public UserDetailService userDetailService(UserRepository userRepository) {
-        return userName -> {
-            Optional<com.rodin.sanitaryEngineeringShop.model.User> user = userRepository.findByUserName(userName);
-            if (user.isPresent()) {
-                return user.get();
-            } else {
-                throw new UsernameNotFoundException("User " + userName + " not found");
-            }
-        };
     }
 }
